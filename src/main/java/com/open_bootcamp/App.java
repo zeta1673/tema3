556de7d67e85c8d5da983214870599d415af1f36
Ricardo Valdivieso <valdivieso.ricardo@yahoo.es>
2022-05-19 21:16:32 -0500

    first commit

---
+++ src/main/java/com/open_bootcamp/App.java
@@ -0,0 +1,30 @@
+package com.open_bootcamp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        suma(2,5,6);
+
+        Coche miCoche = new Coche();
+
+        miCoche.sumarPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+    public static void suma(int a, int b, int c){
+        int suma = a + b + c;
+
+        System.out.println(suma);
+    }
+
+    static class Coche{
+        public int puertas =0;
+
+        public void sumarPuertas(){
+            this.puertas++;
+        }
+    }
+}
